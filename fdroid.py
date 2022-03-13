b8d97b9750175b0e090efd5adade39cb3998d219
kshib <ksyko@pm.me>
2022-03-14 00:10:38 +0530

    add docstrings

--- fdroid.py
+++ fdroid.py
@@ -6,7 +6,15 @@ from bs4 import BeautifulSoup as bs
 from package import Package
 
 
-def search(query: str) -> list:
+def search(query: str) -> list[Package]:
+    """
+    Performs search for the given query
+        Parameters:
+                query (str): Search term
+
+        Returns:
+                packages (list[Package]): List of Packages found for the query
+    """
     packages = []
     query = encode(query, safe='')
     response = requests.get(f"https://search.f-droid.org/?q={query}").content
@@ -21,19 +29,51 @@ def search(query: str) -> list:
     return packages
 
 
-def versions(package):
+def versions(package: Package) -> list:
+    """
+    Finds all versions for given package
+        Parameters:
+                package (Package): Package
+
+        Returns:
+                packages (list): List of suggested and latest versions for the given package
+    """
     return requests.get(f"https://f-droid.org/api/v1/packages/{package.id_}").json()["packages"]
 
 
-def latest_version(package):
+def latest_version(package: Package) -> int:
+    """
+    Finds latest version for given package
+        Parameters:
+                package (Package): Package
+
+        Returns:
+                str : Returns latest version Code
+    """
     return versions(package)[0]["versionCode"]
 
 
-def suggested_version(package):
+def suggested_version(package: Package) -> int:
+    """
+    Returns suggested version for given package
+        Parameters:
+                package (Package): Package
+
+        Returns:
+                str : Returns suggested version code
+    """
     return requests.get(f"https://f-droid.org/api/v1/packages/{package.id_}").json()["suggestedVersionCode"]
 
 
-def version_code(package, version_name):
+def version_code(package: Package, version_name: str) -> int:
+    """
+    Returns corresponding version code for version name
+        Parameters:
+                package (Package): Package
+                version_name (str): Version name of the package
+        Returns:
+                int : Returns version Code
+    """
     for version in versions(package):
         if version_name == version["versionName"]:
             return version["versionCode"]
@@ -41,5 +81,8 @@ def version_code(package, version_name):
 
 # TODO
 # noinspection PyUnusedLocal
-def add_repo(package):
+def add_repo(repo):
+    """
+    Add repo to check to search packages from
+    """
     pass
--- installer.py
+++ installer.py
@@ -3,7 +3,16 @@ from user import *
 from util import *
 
 
-def download(package, code):
+def download(package: Package, code: int = 0) -> None:
+    """
+    Download package to default download directory
+
+    :param package: Package
+    :type package: Package
+    :param code: Version code of the package to download
+    :type code: int
+    """
+
     if code == 0:
         code = suggested_version(package)
     file_name = f"{package.id_}_{code}.apk"
@@ -13,15 +22,30 @@ def download(package, code):
     open(f'{dl_dir}/{file_name}', 'wb').write(apk.content)
 
 
-def suggested_outdated(package):
+def suggested_outdated(package: Package) -> bool:
+    """
+    :param package: Package
+    :type package: Package
+    :return: True if version for given package is lower than the suggested version
+    """
     return installed_package_version(package) < suggested_version(package)
 
 
 def latest_outdated(package):
+    """
+    :param package: Package
+    :type package: Package
+    :return: True if version for given package is lower than the latest version
+    """
     return installed_package_version(package) < latest_version(package)
 
 
-def outdated_packages(suggested=True):
+def outdated_packages(suggested: bool = True) -> list[Package]:
+    """
+    :param suggested: Use suggested version? Latest otherwise
+    :type suggested: bool
+    :return: Returns list of outdated packages
+    """
     packages = []
     for package in installed_packages('fdroid'):
         if suggested and suggested_outdated(package):
@@ -31,7 +55,14 @@ def outdated_packages(suggested=True):
     return packages
 
 
-def update_outdated_packages(suggested=True):
+def update_outdated_packages(suggested=True) -> list[Package]:
+    """
+    Downloads and installs outdated packages
+
+    :param suggested: Use suggested version? Latest otherwise
+    :type suggested: bool
+    :return: List of packages failed to install
+    """
     packages = []
     for package in outdated_packages():
         if suggested:
@@ -39,12 +70,18 @@ def update_outdated_packages(suggested=True):
         else:
             code = latest_version(package)
         download(package, code)
-        install(package, code)
+        success = install(package, code)
+        if not success:
+            packages.append(package)
     return packages
 
 
-# returns version code of installed package
 def installed_package_version(package: Package) -> int:
+    """
+    :param package: Package
+    :type package: Package
+    :return: Installed package version
+    """
     if adb_connected():
         try:
             output = command(f"adb shell dumpsys package {package.id_} | grep versionName")
@@ -55,11 +92,27 @@ def installed_package_version(package: Package) -> int:
     return -1
 
 
-def search_install(query):
-    return install(search(query)[0])
+def search_install(query: str) -> bool:
+    """
+    Searches packages matching given query and installs first result
+    :param query: Search term
+    :type query: Package
+    :return: True if install was successful
+    """
+    results = search(query)
+    if results:
+        return install(results[0])
 
 
-def install(package, code=0, user=0):
+def install(package:Package, code:int=0, user:int=0) -> bool:
+    """
+    Install package
+
+    :param package: Package
+    :param code: Version of package to install
+    :param user: User id. 0 by default (main user)
+    :return: True if install was successful
+    """
     if code == 0:
         code = latest_version(package)
     dl_dir = download_dir()
@@ -79,7 +132,14 @@ def install(package, code=0, user=0):
     return False
 
 
-def uninstall(package, user=0):
+def uninstall(package: Package, user=0):
+    """
+    Uninstall package
+
+    :param package: Package
+    :param user: User id. 0 by default (main user)
+    :return: True if install was successful
+    """
     if adb_connected():
         user = f"--user {user}"
         id_ = f"{package.id_}"
@@ -89,3 +149,4 @@ def uninstall(package, user=0):
             return "Success" in output
         except subprocess.CalledProcessError as e:
             print(f"Failed to uninstall'{package.name}'", e.output)
+    return False
