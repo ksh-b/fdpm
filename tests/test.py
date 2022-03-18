a1ec13cb7eb81d7f4dd37bd70d2e57266836d629
kshib <ksyko@pm.me>
2022-03-18 23:45:39 +0530

    uncomment

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from models.installer import search_install
+from models.installer import search_install, uninstall
 from models.package import Package
 from models.user import package_installed
 
@@ -6,4 +6,4 @@ package = Package(id_="org.woheller69.eggtimer")
 app = package.id_
 assert search_install(app)
 assert package_installed(package)
-# assert uninstall(package)
+assert uninstall(package)
