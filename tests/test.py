e75ece5fb260928703f9c937d5b6eb1446ed80f4
kshib <ksyko@pm.me>
2022-03-22 11:16:22 +0530

    move

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
-from models.fdroid import search
-from models.installer import install_all, uninstall_all
+from fdpm.models.fdroid import search
+from fdpm.models import install_all, uninstall_all
 
 ids=["com.donnnno.arcticons","com.donnnno.arcticons.light"]
 assert "A monotone line-based icon pack" in str(search(ids[0].split(".")[-1]))
