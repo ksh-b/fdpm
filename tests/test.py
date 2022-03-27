9f240d5fd3bdf086827793b031aa408714dba35c
kshib <ksyko@pm.me>
2022-03-27 12:50:46 +0530

    remove test for now

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-from fdpm.models.fdroid import search
-from fdpm.models import install_all, uninstall_all
-
-ids=["com.donnnno.arcticons","com.donnnno.arcticons.light"]
-assert "A monotone line-based icon pack" in str(search(ids[0].split(".")[-1]))
-install_all(ids)
-uninstall_all(ids)
